35f789bd3ddbec2291d7859f6302b1eaba24fb8a
Akshat Zala <akshat.z@arnekt.com>
2020-08-11 19:57:29 +0530

    Added helloworld using Vue.JS

---
+++ demo/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <PostsExample/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import PostsExample from './components/Posts'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    PostsExample
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
