278c496e7a1165d0d964a11c82c09b006c2f9924
Akshat Zala <akshatzala@gmail.com>
2020-08-12 15:05:04 +0530

    Posts pulled from a hyperlink

--- demo/src/components/Posts.vue
+++ demo/src/components/Posts.vue
@@ -1,18 +1,50 @@
 <template>
-<div>
-    <h1>Hello World</h1>
-</div>
-
+    <div class="container">
+        <table class="table table-striped mt-4">
+        <thead>
+            <tr>
+                <th scope="col">userID</th>
+                <th scope="col">ID</th>
+                <th scope="col">Title</th>
+                <th scope="col">Body</th>
+                <!-- <th scope="col">Age</th>
+                <th scope="col">dob</th>
+                <th scope="col">Gender</th>
+                <th scope="col">Blood Group</th> -->
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="post in posts" v-bind:key="posts.Id">
+                <th scope="row">{{post.userID}}</th>
+                <td>{{post.id}}</td>
+                <td>{{post.title}}</td>
+                <td>{{post.body}}</td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
-export default{
-    name : "PostsExample",
-    data(){
-        return {
-            
+    import axios from 'axios'
+    export default{
+        name: 'PostsExample',
+        data(){
+            return {
+                posts:[]
+            }
+        },
+        created(){
+        axios.get('http://jsonplaceholder.typicode.com/posts').then(posts=>(
+
+            this.posts =posts.data
+        
+        ))
+
         }
     }
-}
 </script>
 
+<style>
+
+</style>
