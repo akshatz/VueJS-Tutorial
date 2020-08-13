0bf1d5e42e64b5f600b07c7bb001a5dff248e242
Akshat Zala <akshatzala@gmail.com>
2020-08-13 20:26:06 +0530

    Changed contact.vue style to scoped

--- demo/src/components/Contact.vue
+++ demo/src/components/Contact.vue
@@ -1,15 +1,16 @@
 <template>
-
     <div class="container">Contact Number</div>
-
 </template>
 <script>
 
 </script>  
 
-<style>
+<style scoped>
+
 .container{
-    margin:1px;
+    margin:1% 1%;
+    background-color:#1231 ;        
 }
+
 </style>
 
